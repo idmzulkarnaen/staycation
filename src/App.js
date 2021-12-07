be41e6df62f2890c7260409eb92ff32012ca2b23
idmzulkarnaen <kreatifku1995@gmail.com>
2021-12-07 15:03:13 +0700

    styling navbar header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import "./assets/scss/style.scss"
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./assets/scss/style.scss";
 
 function App() {
-  return (
-    <div className="App">
-      Hai Dunia
-    </div>
-  );
+  return <div className="App">
+    <Router>
+      <Router path="/" component={LandingPage}></Router>
+    </Router>
+  </div>;
 }
 
 export default App;
